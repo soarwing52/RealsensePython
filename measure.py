823b09d93c64cc9b1b79d808af645691914a6aa9
soarwing52 <31880310+soarwing52@users.noreply.github.com>
2019-04-10 17:18:13 +0200

    Add files via upload

---
+++ measure.py
@@ -0,0 +1,99 @@
+import pyrealsense2 as rs
+# Import Numpy for easy array manipulation
+import numpy as np
+# Import OpenCV for easy image rendering
+import cv2
+import sys
+from matplotlib import pyplot as plt
+from matplotlib.widgets2 import Ruler
+import os
+
+
+num = raw_input('Wegenummer:\n')
+file_name = './bag/{}.bag'.format(num)
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_device_from_file(file_name)
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgba8, 30)
+profile = pipeline.start(config)
+device = profile.get_device()
+playback = device.as_playback()
+playback.set_real_time(False)
+align_to = rs.stream.color
+align = rs.align(align_to)
+frame_list = []
+
+with open('./list/{}_matched.txt'.format(num),'r') as csvfile:
+    for line in csvfile:
+        frame = [elt.strip() for elt in line.split(',')]
+        frame_list.append(frame)
+i=0
+try:
+    while True:
+        frames = pipeline.wait_for_frames()
+        aligned_frames = align.process(frames)
+        depth_frame = aligned_frames.get_depth_frame()
+        vard = rs.frame.get_frame_number(depth_frame)
+        if int(vard) == int(frame_list[i][1]) :
+            try:
+                while True:
+                    new_frames = pipeline.wait_for_frames()
+                    color_frame = new_frames.get_color_frame()
+                    var = rs.frame.get_frame_number(color_frame)
+                    if int(var) ==int(frame_list[i][0]):
+                        depth_color = rs.colorizer().colorize(depth_frame)
+                        depth_color_image = np.asanyarray(depth_color.get_data())
+                        color_image = np.asanyarray(color_frame.get_data())
+
+                        color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+                        color_cvt = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+                        print var, vard
+                        img_over = cv2.addWeighted(color_cvt, 1, depth_color_image, 0.3, 0.5)
+                        key = cv2.waitKey(0)
+
+                        xCoord = np.arange(0, 6, 1)
+                        yCoord = [0, 1, -3, 5, -3, 0]
+                        fig = plt.figure()
+                        ax = fig.add_subplot(111)
+                        number = 'frame number: c:{}/d:{}'.format(var,vard)
+                        markerprops = dict(marker='o', markersize=5, markeredgecolor='red')
+                        lineprops = dict(color='red', linewidth=2)
+                        plt.imshow(color_image)
+                        ax.grid(False)
+                        plt.text(500, -20, number, fontsize=15)
+                        figManager = plt.get_current_fig_manager().window.state('zoomed')
+
+                        ruler = Ruler(ax=ax,
+                                      depth_frame=depth_frame,
+                                      color_intrin=color_intrin,
+                                      useblit=True,
+                                      markerprops=markerprops,
+                                      lineprops=lineprops,
+                                      )
+
+                        #print 'show image'
+                        plt.show()
+                        # if pressed escape exit program
+                        if key == 27:
+                            cv2.destroyAllWindows()
+                            break
+                        else:
+                            cv2.destroyAllWindows()
+                            i+= 1
+                            break
+
+            finally:
+                pass
+
+        else:
+            pass
+
+except IndexError:
+    print 'file ended'
+
+
+finally:
+    print 'finish'
+
+
---
+++ png_list_prepare.py
@@ -0,0 +1,154 @@
+import os
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import sys
+from matplotlib import pyplot as plt
+from matplotlib.widgets2 import Ruler
+
+def color_to_png(input_file):
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgba8, 30)
+    config.enable_device_from_file('./bag/'+input_file,False)
+    profile = pipeline.start(config)
+    try:
+        while True:
+            frames = pipeline.wait_for_frames()
+            frames.keep()
+            if frames:
+                color_frame = frames.get_color_frame()
+                var = rs.frame.get_frame_number(color_frame)
+                color_image = np.asanyarray(color_frame.get_data())
+                color_cvt = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+                cv2.namedWindow("Color Stream", cv2.WINDOW_AUTOSIZE)
+                cv2.imshow("Color Stream", color_cvt)
+
+                cv2.imwrite('./png/'+input_file[:-4] + '_c_' + str(var) + '.png', color_cvt,[cv2.IMWRITE_PNG_COMPRESSION,2])
+                key = cv2.waitKey(1)
+                # if pressed escape exit program
+                if key == 27:
+                    cv2.destroyAllWindows()
+                    break
+
+    except RuntimeError:
+        print 'frame covert finished for '+input_file
+        cv2.destroyAllWindows()
+    finally:
+        pass
+
+def color_match_list(input_file):
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_device_from_file('./bag/'+input_file, False)
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgba8, 30)
+    profile = pipeline.start(config)
+    device = profile.get_device()
+    playback = device.as_playback()
+    playback.set_real_time(False)
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+    fold = open('./list/'+input_file[:-4] +'_colorlist.txt', 'w')
+    try:
+        while True:
+            frames = pipeline.wait_for_frames()
+            aligned_frames = align.process(frames)
+            color_frame = aligned_frames.get_color_frame()
+            vard = rs.frame.get_frame_number(color_frame)
+            time_depth = rs.frame.get_timestamp(color_frame)
+            fold.write(str(vard) + ',')
+            fold.write(str(time_depth) + '\n')
+
+    except RuntimeError:
+        print 'color frame list ended'
+    finally:
+        fold.close()
+        pipeline.stop()
+
+def depth_match_list(input_file):
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_device_from_file('./bag/'+input_file, False)
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgba8, 30)
+    profile = pipeline.start(config)
+    device = profile.get_device()
+    playback = device.as_playback()
+    playback.set_real_time(False)
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+    fold = open('./list/'+input_file[:-4] +'_depthlist.txt', 'w')
+    try:
+        while True:
+            frames = pipeline.wait_for_frames()
+            aligned_frames = align.process(frames)
+            depth_frame = aligned_frames.get_depth_frame()
+            vard = rs.frame.get_frame_number(depth_frame)
+            time_depth = rs.frame.get_timestamp(depth_frame)
+            fold.write(str(vard) + ',')
+            fold.write(str(time_depth) + '\n')
+
+    except RuntimeError:
+        print 'depth frame list ended'
+    finally:
+        fold.close()
+        pipeline.stop()
+
+def match_frame_list(input_file):
+    depth_frame_list = []
+    color_frame_list = []
+    with open('./list/'+input_file[:-4]+'_colorlist.txt', 'r') as csvfile:
+        for line in csvfile:
+            frame = [elt.strip() for elt in line.split(',')]
+            color_frame_list.append(frame)
+
+    with open('./list/'+input_file[:-4]+'_depthlist.txt', 'r') as depcsv:
+        for dline in depcsv:
+            frame_d = [dd.strip() for dd in dline.split(',')]
+            depth_frame_list.append(frame_d)
+
+    csvfile.close()
+    depcsv.close()
+
+    f_list = []
+    for t_c in color_frame_list:
+        for t_d in depth_frame_list:
+            gap = float(t_c[1]) - float(t_d[1])
+            gap = abs(gap)
+            if gap < 20:
+                f_list.append(str(t_c[0]) + ',' + str(t_d[0]) + '\n')
+    unique_list = []
+    with open('./list/'+input_file[:-4]+'_matched.txt', 'w') as matched:
+        for elem in f_list:
+            if elem not in unique_list:
+                unique_list.append(elem)
+                matched.write(elem)
+
+    matched.close()
+    print 'finished match list '+input_file
+
+
+dir_name = 'bag'
+if not os.path.exists('png'):
+    try:
+        os.makedirs('png', 0o700)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+if not os.path.exists('list'):
+    try:
+        os.makedirs('list', 0o700)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+for filename in os.listdir(dir_name):
+    print filename
+    color_to_png(filename)
+    color_match_list(filename)
+    depth_match_list(filename)
+    match_frame_list(filename)
+print 'all done, Perfect!'
