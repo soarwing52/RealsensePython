83f2d3e3d374dacd767eb86ebf313e916dcc72c3
Jay <soarwing52hot@gmail.com>
2019-05-29 14:43:48 +0200

    add geotagging

--- shp&png_0527.py
+++ shp&png_0527.py
@@ -1,13 +1,91 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
 import tkFileDialog
-import os
 import arcpy
 from arcpy import management
 import pyrealsense2 as rs
 import numpy as np
 import cv2
 import multiprocessing as mp
+import os
+import piexif
+from fractions import Fraction
+
+def to_deg(value, loc):
+    """convert decimal coordinates into degrees, munutes and seconds tuple
+    Keyword arguments: value is float gps-value, loc is direction list ["S", "N"] or ["W", "E"]
+    return: tuple like (25, 13, 48.343 ,'N')
+    """
+    if value < 0:
+        loc_value = loc[0]
+    elif value > 0:
+        loc_value = loc[1]
+    else:
+        loc_value = ""
+    abs_value = abs(value)
+    deg =  int(abs_value)
+    t1 = (abs_value-deg)*60
+    min = int(t1)
+    sec = round((t1 - min)* 60, 5)
+    return (deg, min, sec, loc_value)
+
+
+def change_to_rational(number):
+    """convert a number to rantional
+    Keyword arguments: number
+    return: tuple like (1, 2), (numerator, denominator)
+    """
+    f = Fraction(str(number))
+    return (f.numerator, f.denominator)
+
+
+def set_gps_location(file_name, lat, lng, altitude):
+    """Adds GPS position as EXIF metadata
+    Keyword arguments:
+    file_name -- image file
+    lat -- latitude (as float)
+    lng -- longitude (as float)
+    altitude -- altitude (as float)
+    """
+    lat_deg = to_deg(lat, ["S", "N"])
+    lng_deg = to_deg(lng, ["W", "E"])
+
+    exiv_lat = (change_to_rational(lat_deg[0]), change_to_rational(lat_deg[1]), change_to_rational(lat_deg[2]))
+    exiv_lng = (change_to_rational(lng_deg[0]), change_to_rational(lng_deg[1]), change_to_rational(lng_deg[2]))
+
+    gps_ifd = {
+        piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+        piexif.GPSIFD.GPSAltitudeRef: 1,
+        piexif.GPSIFD.GPSAltitude: change_to_rational(round(altitude)),
+        piexif.GPSIFD.GPSLatitudeRef: lat_deg[3],
+        piexif.GPSIFD.GPSLatitude: exiv_lat,
+        piexif.GPSIFD.GPSLongitudeRef: lng_deg[3],
+        piexif.GPSIFD.GPSLongitude: exiv_lng,
+    }
+
+    exif_dict = {"GPS": gps_ifd}
+    exif_bytes = piexif.dump(exif_dict)
+    piexif.insert(exif_bytes, file_name)
+
+def geotag():
+    global project_dir,t
+    fotolog = project_dir + '/shp/matcher.txt'
+    png_dir = project_dir + '/png'
+    with open(fotolog, 'r') as log:
+        log.readline()
+        for y in log:
+            data = y.split(',')
+            weg_num, frame, Lat, Lon = data[0], data[2], float(data[4]), float(data[5])
+            jpgname = '{}/{}-{}.jpg'.format(png_dir, weg_num, frame)
+            try:
+                set_gps_location(jpgname, Lat, Lon, 0)
+            except IOError:
+                print jpgname + ' not found'
+                continue
+            finally:
+                pass
+    print 'tagged'
+    t.insert(END, 'geotagging fertig\n')
 
 
 def test_bag(input_file):
@@ -253,7 +331,7 @@ def color_to_png(input_file):
             var = rs.frame.get_frame_number(color_frame)
             color_image = np.asanyarray(color_frame.get_data())
             color_cvt = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-            png_name = '{}/png/{}-{}.png'.format(projectdir, bagname[:-4], var)
+            png_name = '{}/png/{}-{}.jpg'.format(projectdir, bagname[:-4], var)
             # print png_name
             exist = os.path.isfile(png_name)
             if exist:
@@ -328,7 +406,8 @@ def main():
     t.pack()
 
     Button(frame_2, text='generage shp', command=from_bag_to_list).grid(row=1, column=1)
-    Button(frame_2, text='generate png', command=lambda: test_png()).grid(row=1, column=2)
+    Button(frame_2, text='generate jpg', command=lambda: test_png()).grid(row=1, column=2)
+    Button(frame_2, text='geotag', command=geotag).grid(row=1, column=3)
 
 
     root.mainloop()
