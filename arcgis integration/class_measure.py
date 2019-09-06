c282e8b00765154d5f0588beed97988490e774d9
Jay <soarwing52hot@gmail.com>
2019-09-06 08:59:55 +0200

    improve search
    smooth the search progress
    forward/backward
    
    code clean

--- arcgis integration/class_measure.py
+++ arcgis integration/class_measure.py
@@ -13,21 +13,23 @@ class Arc_Real:
         self.file_dir = os.path.dirname(self.BagFilePath)
         self.Pro_Dir = os.path.dirname(self.file_dir)
         self.file_name = '{}\\bag\\{}.bag'.format(self.Pro_Dir,self.weg_id)
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_device_from_file(self.file_name)
+        self.command = {'q': 0, 'left': -1, 'right': 1}
+
 
     def video(self):
         try:
-            pipeline = rs.pipeline()
-            config = rs.config()
-            config.enable_device_from_file(self.file_name)
-            config.enable_stream(rs.stream.color, 1920, 1080, rs.format.rgb8, 30)
-            profile = pipeline.start(config)
+            self.config.enable_stream(rs.stream.color, 1920, 1080, rs.format.rgb8, 30)
+            profile = pipeline.start(self.config)
             device = profile.get_device()
             playback = device.as_playback()
             playback.set_real_time(False)
 
             while True:
                 pause = False
-                frames = pipeline.wait_for_frames()
+                frames = self.pipeline.wait_for_frames()
                 color_frame = frames.get_color_frame()
                 if not color_frame:
                     continue
@@ -66,132 +68,124 @@ class Arc_Real:
         finally:
             print 'finish'
             os._exit(0)
-            pipeline.stop()
+            self.pipeline.stop()
 
-    def measure(self,frame_id):
-        pipeline = rs.pipeline()
-        config = rs.config()
-        config.enable_device_from_file(self.file_name)
-        config.enable_all_streams()
-        profile = pipeline.start(config)
+    def measure(self,object_id):
+
+        self.config.enable_all_streams()
+
+        profile = self.pipeline.start(self.config)
 
         device = profile.get_device()
         playback = device.as_playback()
         playback.set_real_time(False)
-
-        align_to = rs.stream.color
-        align = rs.align(align_to)
         frame_list = []
 
-        with open('{}\\foto_log\\{}.txt'.format(self.Pro_Dir, self.weg_id), 'r') as csvfile:
+        with open('all.csv'.format(self.Pro_Dir), 'r') as csvfile:
             for line in csvfile:
-                frame = [elt.strip() for elt in line.split(',')]
+                frame = [elt.strip() for elt in line.split(';')]
                 frame_list.append(frame)
 
-        global i
-        i = int(frame_id)-1
+        for obj in frame_list:
+            if obj[0] == object_id:
+                self.i = frame_list.index(obj)
+                break
+
+        self.search_count = 0
 
         try:
             while True:
-                x = i
-                frames = pipeline.wait_for_frames()
-                aligned_frames = align.process(frames)
-                depth_frame = aligned_frames.get_depth_frame()
-                if not depth_frame:
-                    continue
-                depth_color_frame = rs.colorizer().colorize(depth_frame)
-                vard = rs.frame.get_frame_number(depth_frame)
-                if abs(int(vard) - int(frame_list[i][2]))<5:
-                    print 'match depth{}'.format(vard)
-                    try:
-                        while True:
-                            new_frames = pipeline.wait_for_frames()
-                            new_aligned_frames = align.process(new_frames)
-                            color_frame = new_aligned_frames.get_color_frame()
-                            if not color_frame:
-                                continue
-                            var = rs.frame.get_frame_number(color_frame)
-                            if abs(int(var) - int(frame_list[i][1]))<5:
-                                print 'match color{}'.format(var)
-                                color_image = np.asanyarray(color_frame.get_data())
-                                color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
-                                fig = plt.figure()
-                                fig.canvas.mpl_disconnect(fig.canvas.manager.key_press_handler_id)
-                                ax = fig.add_axes([0, 0, 1, 1])
-                                number = 'Wegnummer:{}\nFrame: c:{}/d:{}'.format(self.weg_id, str(var), str(vard))
-                                plt.imshow(color_image)
-                                ax.grid(False)
-                                fig.suptitle(number, fontsize=20)
-                                markerprops = dict(marker='o', markersize=5, markeredgecolor='red')
-                                lineprops = dict(color='red', linewidth=2)
-                                figManager = plt.get_current_fig_manager().window.state('zoomed')
-                                ruler = Ruler(ax=ax,
-                                              depth_frame=depth_frame,
-                                              color_intrin=color_intrin,
-                                              useblit=True,
-                                              markerprops=markerprops,
-                                              lineprops=lineprops,
-                                              )
+                x, depth_num , color_num = self.i, frame_list[self.i][6], frame_list[self.i][5]
 
-                                def search(frame_num):
-                                    a = frame_num
-                                    for x in frame_list:
-                                        ans = a in x
-                                        if ans == True:
-                                            qq = frame_list.index(x)
-                                            return int(qq)
-                                        else:
-                                            print 'no frame {} founded'.format(a)
-                                            pass
+                depth_frame = self.frame_getter('depth', depth_num)
+                color_frame = self.frame_getter('color', color_num)
 
-                                def quit_figure(event):
-                                    global i
-                                    if event.key == 'q':
-                                        i = 800
-                                        plt.close(event.canvas.figure)
-                                        return i
-                                    elif event.key == 'left':
-                                        plt.close(event.canvas.figure)
-                                        i -= 1
-                                    elif event.key == 'right':
-                                        plt.close(event.canvas.figure)
-                                        i += 1
-                                    elif event.key == 'up':
-                                        plt.close(event.canvas.figure)
-                                        frame_num = raw_input('frame numer: ')
-                                        i = search(frame_num)
+                color_image = np.asanyarray(color_frame.get_data())
+                color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+                fig = plt.figure()
+                fig.canvas.mpl_disconnect(fig.canvas.manager.key_press_handler_id)
+                ax = fig.add_axes([0, 0, 1, 1])
+                number = 'Wegnummer:{}\nFrame: c:{}/d:{}'.format(self.weg_id, self.color_frame_num, self.depth_frame_num)
+                plt.imshow(color_image)
+                ax.grid(False)
+                fig.suptitle(number, fontsize=20)
+                markerprops = dict(marker='o', markersize=5, markeredgecolor='red')
+                lineprops = dict(color='red', linewidth=2)
+                figManager = plt.get_current_fig_manager().window.state('zoomed')
+                ruler = Ruler(ax=ax,
+                              depth_frame=depth_frame,
+                              color_intrin=color_intrin,
+                              useblit=True,
+                              markerprops=markerprops,
+                              lineprops=lineprops,
+                              )
 
-                                cid = plt.gcf().canvas.mpl_connect('key_press_event', quit_figure)
-                                plt.xlim((-80,2000))
-                                plt.ylim((1100,-100))
-                                plt.show()
-                                if x == i:
-                                    i = 800
-                                break
-                            else:
-                                print 'searching for RGB image: {}, now:{}'.format(frame_list[i][1], var)
+                cid = plt.gcf().canvas.mpl_connect('key_press_event', self.quit_figure)
+                plt.xlim((-80,2000))
+                plt.ylim((1100,-100))
+                plt.show()
+                if x == self.i: # close script if close window or press q
+                    os._exit(0)
 
-                    finally:
-                        pass
-                elif i == 800:
-                    break
-                else:
-                    print 'searching for Depth image: {}, now:{}'.format(frame_list[i][2], vard)
-                    pass
 
         except IndexError:
             print 'file ended'
+
         finally:
-            print 'return'
+            print 'finally'
             os._exit(0)
-            pipeline.stop()
-            return
+
+
+    def frame_getter(self, type, num):
+        align_to = rs.stream.color
+        align = rs.align(align_to)
+        try:
+            while True:
+                frames = self.pipeline.wait_for_frames()
+                aligned_frames = align.process(frames)
+                if type == 'color':
+                    frame = aligned_frames.get_color_frame()
+                    self.color_frame_num = frame.get_frame_number()
+                else:
+                    frame = aligned_frames.get_depth_frame()
+                    self.depth_frame_num = frame.get_frame_number()
+
+                if not frame:
+                    continue
+                frame_num = rs.frame.get_frame_number(frame)
+
+                if abs(int(frame_num) - int(num)) < 5:
+                    print 'match {} {}'.format(type, frame_num)
+                    self.search_count = 0
+                    break
+
+        finally:
+            return frame
+
+    def count_search(self, now, target):
+        if abs(now - target) < 100:
+            self.search_count += 1
+            if self.search_count > 10:
+                self.i += self.command[event.key]
+
+    def quit_figure(self, event):
+        """
+        keyboard command for matplotlib
+        :param event:
+        :return:
+        """
+        if event.key:
+            plt.close(event.canvas.figure)
+            self.direction = event.key
+            self.i += self.command[event.key]
+
+
 
 
 def main():
-    a = Arc_Real(r'C:\Users\cyh\Desktop\mittel\png\0513_1.png','0513_1')
+    a = Arc_Real(r'X:/Mittelweser/0612/jpg/0612_003-511.jpg','0612_003')
     #a.video()
-    a.measure('5')
+    a.measure('5793')
     print a
 
 if __name__ == '__main__':
